c6c0386135d073a399e4a6bbf3ff5b95b8a692dd
wszxlsun <wszxlsun@gmail.com>
2025-01-25 19:03:48 +0800

    finished

--- code.s
+++ code.s
@@ -6,7 +6,7 @@ _start: addi  a0, x0, 1      # 1 = StdOut
         addi  a7, x0, 64     # linux write system call
         ecall                # Call linux to output the string
 
-        addi    a0, x0, 0   # Use 0 return code
+        addi    a0, x0, 1   # Use 0 return code
         addi    a7, x0, 93  # Service command code 93 terminates
         ecall               # Call linux to terminate the program
 
